46eb16e6dcff4c189fdaff48bc3e51525c8a93f4
Joshua Hull <joshbuddy@gmail.com>
2009-03-19 15:00:48 -0400

    more complete esi view impl. readme

--- lib/spackle/view.rb
+++ lib/spackle/view.rb
@@ -15,6 +15,30 @@ module Spackle
       end
     end
 
+    def esi_vars
+      concat("<esi:vars>")
+      yield
+      concat("</esi:vars>")
+    end
+
+    def esi_remove
+      concat("<esi:remove>")
+      yield
+      concat("</esi:remove>")
+    end
+
+    def esi_comment
+      concat("<!--esi")
+      yield
+      concat("-->")
+    end
+
+    def esi_choose
+      concat("<esi:choose>")
+      yield
+      concat("</esi:choose>")
+    end
+
     def esi_try(*args, &blk)
       esi_tag('try', *args, &blk)
     end
@@ -27,21 +51,51 @@ module Spackle
       esi_tag('except', *args, &blk)
     end
     
-    def esi_include(name, *args)
-      path_parts = ActionController::Base.esi_paths[name][:path].split('/').select{|p| not p.blank?}
-      path_parts.collect! do |p|
-        if p[0] == ?:
-          case args.last 
-          when Hash
-            args.last.delete(p[1,p.size-1].to_sym)
+    def esi_url(name, *args)
+      case name 
+      when String
+        name
+      when Symbol
+        opts = args.last.is_a?(Hash) ? args.last : nil
+        onerror = opts && opts.delete(:onerror) || 'continue'
+        
+        path_parts = ActionController::Base.esi_paths[name][:path].split('/').select{|p| not p.blank?}
+        path_parts.collect! do |p|
+          if p[0] == ?:
+            if opts
+              opts.delete(p[1, p.size - 1].to_sym)
+            else
+              args.shift
+            end or raise "the variable #{p} references by your esi method needs to be given a value"
           else
-            args.shift
-          end or raise "the variable #{p} references by your esi method needs to be given a value"
-        else
-          p
+            p
+          end
         end
+        
+        spackle_esi_url(:controller => ActionController::Base.esi_paths[name][:controller], :action => name) + '/' + (path_parts * '/')
+      else
+        raise
       end
-      %{<esi:include src="#{spackle_esi_url(:controller => ActionController::Base.esi_paths[name][:controller], :action => name) + '/'+ path_parts * '/'}"/>}
+    end
+    
+    def esi_src(name, *args)
+      %|src="#{esi_url(name, *args)}" |
+    end
+    
+    def esi_alt(name, *args)
+      %|alt="#{esi_url(name, *args)}" |
+    end
+    
+    def esi_include(*args)
+      if block_given?
+        concat("<esi:include ")
+        yield
+        concat(" />")
+      elsif args.first
+        %|<esi:include #{esi_src(args.first, *args[1, args.size - 1])}/>|
+      else
+        raise "include needs name or block"
+      end
     end
     
   end
